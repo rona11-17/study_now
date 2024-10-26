9621577a689332ca6bc0c82b424718126b6d66c8
root <root@chestnuts166.localdomain>
2024-10-26 15:54:55 +0900

    make features of login and auth

---
+++ app/controllers/mypage_controller.rb
@@ -0,0 +1,6 @@
+class MypageController < ApplicationController
+  before_action :authenticate
+
+  def index
+  end
+end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "firebase/app", to: "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js"
+pin "firebase/auth", to: "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js"
+pin "env", to: "env.js"
