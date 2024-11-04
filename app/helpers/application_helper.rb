f6e38248738ffaa1e4eb06beb2b554d0df552c38
raimu <raijirou12@gmail.com>
2024-11-04 14:58:40 +0900

    タブなどのUIの強化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def active_link_class(controller, action)
+        "font-weight-bold underline" if controller_name == controller && action_name == action
+      end
 end
